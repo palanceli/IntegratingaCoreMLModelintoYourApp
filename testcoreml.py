c474f9da8081e916ce057ec568e951ccb7641dbe
palance <palanceli@163.com>
2017-08-26 22:39:57 +0800

    commit changes

--- testcoreml.py
+++ testcoreml.py
@@ -11,17 +11,22 @@ if __name__ == '__main__':
     
     modelFilePath = '%s/%s' % (os.getcwd(), 'MarsHabitatPricePredictor/Resources/MarsHabitatPricer.mlmodel')
     logging.debug(modelFilePath)
-    model = coremltools.models.MLModel(modelFilePath)  # 
+    model = coremltools.models.MLModel(modelFilePath)  # 加载mlmodel文件
 
+    # 打印各字段，这些是文件的概要信息
     logging.info('author:              %s' % (model.author))
     logging.info('license:             %s' % (model.license))
     logging.info('short description:   %s' % (model.short_description))
     logging.info('input description:   %s' % (model.input_description))
     logging.info('output description:  %s' % (model.output_description))
  
+ 	# 打印spec，这里有详细的各字段信息
     logging.info(model.get_spec())
 
-    data = {'solarPanels':1.0, 'greenhouses':1.0, 'size':1024}
-    predictions = model.predict(data)
-    logging.info('predictions:         %d' % predictions['price'])
+    # 根据输入的三个字段，验证输出值
+    dataList = [{'solarPanels':1.0, 'greenhouses':1.0, 'size':1024},
+                {'solarPanels':4.0, 'greenhouses':5.0, 'size':10004}]
+    for dataItem in dataList:
+    	predictions = model.predict(dataItem)
+        logging.info('predictions:         %d' % predictions['price'])
 
