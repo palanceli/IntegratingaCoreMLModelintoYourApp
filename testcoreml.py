9d69e52df6d1b1a6c7825159fe89bd2c8a73a54e
palanceli <palanceli@163.com>
2017-08-26 16:43:51 +0800

    commit changes

--- testcoreml.py
+++ testcoreml.py
@@ -1,18 +1,23 @@
 # -*- coding:utf-8 -*-
 
 import coremltools
+import logging
 
-modelFilePath = '/Users/palance/Downloads/IntegratingaCoreMLModelintoYourApp/MarsHabitatPricePredictor/Resources/MarsHabitatPricer.mlmodel'
-model = coremltools.models.MLModel(modelFilePath)
+if __name__ == '__main__':
+    loggingFormat = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=loggingFormat, datefmt='%H:%M',)
+	    
+    modelFilePath = 'MarsHabitatPricePredictor/Resources/MarsHabitatPricer.mlmodel'
+    model = coremltools.models.MLModel(modelFilePath)
 
-print('author:              %s' % (model.author))
-print('license:             %s' % (model.license))
-print('short description:   %s' % (model.short_description))
-print('input description:   %s' % (model.input_description))
-print('output description:  %s' % (model.output_description))
+    print('author:              %s' % (model.author))
+    print('license:             %s' % (model.license))
+    print('short description:   %s' % (model.short_description))
+    print('input description:   %s' % (model.input_description))
+    print('output description:  %s' % (model.output_description))
 
-print(model.get_spec())
+    print(model.get_spec())
 
-data = {'solarPanels':1.0, 'greenhouses':1.0, 'size':1024}
-predictions = model.predict(data)
-print('predictions:         %d' % data.price)
+    data = {'solarPanels':1.0, 'greenhouses':1.0, 'size':1024}
+    predictions = model.predict(data)
+    print('predictions:         %d' % data.price)
